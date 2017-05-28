c735bcb17bdedcc5f722db0f659cee80e2133a6a
anphycn <anphycn@gmail.com>
2017-05-28 15:38:11 +0800

    1

--- controllerModule/src/main/java/com/spring/boot/controller/App3Controller.java
+++ controllerModule/src/main/java/com/spring/boot/controller/App3Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class App3Controller {
 
     @RequestMapping("/hello")
-    public String hello3() {
+    public String hello() {
         return "hello world";
     }
     @RequestMapping("/hello2")
